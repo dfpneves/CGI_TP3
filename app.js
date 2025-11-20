281e88708ad038339403183a2ce299a0583bfb5a
pedrogomes-05 <pca.gomes@campus.fct.unl.pt>
2025-11-20 19:04:26 +0000

    Created objects and gui

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ import * as dat from '../../libs/dat.gui.module.js';
 
 import * as CUBE from '../../libs/objects/cube.js';
 import * as SPHERE from '../../libs/objects/sphere.js';
-
+import * as BUNNY from '../../libs/objects/bunny.js';
+import * as TORUS from '../../libs/objects/torus.js';
 import * as STACK from '../../libs/stack.js';
 
-function setup(shaders) {
-    const canvas = document.getElementById('gl-canvas');
-    const gl = setupWebGL(canvas);
 
-    CUBE.init(gl);
-    SPHERE.init(gl);
+let gl;
+let canvas;
+let program;
+let mView;
+let sceneConfigurationObject = {};
+let datGuiConfigurationObject = {};
 
-    const program = buildProgramFromSources(gl, shaders['shader.vert'], shaders['shader.frag']);
-
-    // Camera  
-    let camera = {
+function defineCamera(gui){
+     // Camera  
+    sceneConfigurationObject.camera = {
         eye: vec3(0, 0, 5),
         at: vec3(0, 0, 0),
         up: vec3(0, 1, 0),
@@ -26,56 +27,19 @@ function setup(shaders) {
         aspect: 1, // Updated further down
         near: 0.1,
         far: 20
-    }
+    };
 
-    // options
-    let options = {
-        wireframe: false,
-        normals: true
-    }
-
-    // lights
-    let lights = [
-        // light 1
-        {
-            position: {
-                x: 0,
-                y: 0,
-                z: 0
-            },
-            intensities: {
-                ambient: [0.0,0.0,0.0],
-                diffuse: [0.0,0.0,0.0],
-                specular: [0.0,0.0,0.0]
-            },
-            axis: {
-                x: 0,
-                y: 0,
-                z: 0
-            },
-            aperture: 0.0,
-            cutoff: 0.0
-        }
-        // light 2
-    ]
-
-    const gui = new dat.GUI();
-
-    // options gui
-    const optionsGui = gui.addFolder("options");
-    optionsGui.add(options, "wireframe");
-    optionsGui.add(options, "normals");
-
+    const camera = sceneConfigurationObject.camera;
 
     // camera gui
     const cameraGui = gui.addFolder("camera");
-    cameraGui.add(camera, "fovy").min(1).max(179).step(1).listen();
-    cameraGui.add(camera, "aspect").min(0).max(10).step(0.01).listen().domElement.style.pointerEvents = "none";
+    datGuiConfigurationObject.cameraGui = cameraGui;
 
+    cameraGui.add(camera, "fovy").min(1).max(179).step(1).listen();
+    cameraGui.add(camera, "aspect").min(0).max(10).step(0.01).listen();
     cameraGui.add(camera, "near").min(0.1).max(20).step(0.01).listen().onChange(function (v) {
         camera.near = Math.min(camera.far - 0.5, v);
     });
-
     cameraGui.add(camera, "far").min(0.1).max(20).step(0.01).listen().onChange(function (v) {
         camera.far = Math.max(camera.near + 0.5, v);
     });
@@ -95,16 +59,317 @@ function setup(shaders) {
     up.add(camera.up, 1).step(0.05).listen().domElement.style.pointerEvents = "none";;
     up.add(camera.up, 2).step(0.05).listen().domElement.style.pointerEvents = "none";;
 
-    // lights gui
-    const lightsGui = gui.addFolder("lights");
-    const light1Gui = lightsGui.addFolder("light1");
-    const lightPositionGui = light1Gui.addFolder("position");
-    light1Gui.add(lights[0], "aperture");
-    light1Gui.add(lights[0], "cutoff");  
+}
 
+function defineOptions(gui, gl){
+    sceneConfigurationObject.options = {
+        backfaceCulling: false,
+        depthTest: true
+    }
+
+    // options gui
+    const options = sceneConfigurationObject.options;
+    const optionsGui = gui.addFolder("options");
+    optionsGui.add(options, "backfaceCulling").name("backface culling").onChange(function(v){
+        (v) ?  gl.enable(gl.CULL_FACE) : gl.disable(gl.CULL_FACE); // enable or disable CULL_FACE
+    });
+    optionsGui.add(options, "depthTest").name("depth test").onChange(function(v){
+        (v) ? gl.enable(gl.DEPTH_TEST) : gl.disable(gl.DEPTH_TEST); 
+    });
+
+    if (options.depthTest) gl.enable(gl.DEPTH_TEST);
+    if (options.backfaceCulling) gl.enable(gl.CULL_FACE);
+    
+}
+
+function initLights(){
+    sceneConfigurationObject.lights = [
+        // Light 1
+        {
+            position: { x: 0.0, y: 0.0, z: 0.0, w: 0.0 }, 
+            intensities: {
+                ambient: [0.0, 0.0, 0.0],
+                diffuse: [0.0, 0.0, 0.0],
+                specular: [0.0, 0.0, 0.0]
+            },
+            axis: { x: 0.0, y: 0.0, z: 0.0 },
+            aperture: 0.0, 
+            cutoff: 0.0      
+        },
+        // Light 2
+        {
+            position: { x: 0.0, y: 0.0, z: 0.0, w: 0.0 }, 
+            intensities: {
+                ambient: [0.0, 0.0, 0.0],
+                diffuse: [0.0, 0.0, 0.0],
+                specular: [0.0, 0.0, 0.0]
+            },
+            axis: { x: 0.0, y: 0.0, z: 0.0 }, 
+            aperture: 0.0, 
+            cutoff: 0.0
+        },
+        // Light 3
+        {
+            position: { x: 0.0, y: 0.0, z: 0.0, w: 0.0 }, 
+            intensities: {
+                ambient: [0.0, 0.0, 0.0],
+                diffuse: [0.0, 0.0, 0.0],
+                specular: [0.0, 0.0, 0.0]
+            },
+            axis: { x: 0.0, y: 0.0, z: 0.0 }, 
+            aperture: 0.0, 
+            cutoff: 0.0
+        }
+    ]
+}
+
+function updateLightIntensity(index, type, color){
+    //TODO
+}
+
+function makeLightGui(lightGui, light, index){
+
+    const lightPositionGui = lightGui.addFolder("position");
+    lightPositionGui.add(light.position, "x").min(-10).max(10).step(0.1).listen();
+    lightPositionGui.add(light.position, "y").min(-10).max(10).step(0.1).listen();
+    lightPositionGui.add(light.position, "z").min(-10).max(10).step(0.1).listen();
+    lightPositionGui.add(light.position, "w").min(0).max(1).step(1).listen();
+
+    const lightColors = {
+            ambient: light.intensities.ambient.map(c => c * 255),
+            diffuse: light.intensities.diffuse.map(c => c * 255),
+            specular: light.intensities.specular.map(c => c * 255),
+    };
+    const lightIntensitiesGui = lightGui.addFolder("intensities");
+    lightIntensitiesGui.addColor(lightColors, "ambient").onChange(v => updateLightIntensity(index, "ambient", v));
+    lightIntensitiesGui.addColor(lightColors, "diffuse").onChange(v => updateLightIntensity(index, "diffuse", v));
+    lightIntensitiesGui.addColor(lightColors, "specular").onChange(v => updateLightIntensity(index, "specular", v));
+
+    const lightAxisGui = lightGui.addFolder("axis");
+    lightAxisGui.add(light.axis, "x").min(-1).max(1).step(0.01);
+    lightAxisGui.add(light.axis, "y").min(-1).max(1).step(0.01);
+    lightAxisGui.add(light.axis, "z").min(-1).max(1).step(0.01);
+
+    lightGui.add(light, "aperture").min(0).max(90).step(1);
+    lightGui.add(light, "cutoff").min(0).max(1).step(0.01);  
+
+}
+
+function defineLights(gui){
+    initLights();
+    const lights = sceneConfigurationObject.lights;
+    const lightsGui = gui.addFolder("lights");
+    lights.forEach((light, index) => {
+        let lightNum = index + 1;
+        const lightGui = lightsGui.addFolder('light' + lightNum);
+        makeLightGui(lightGui, light, index);
+    });
+
+}
+
+function updateMaterialColor(){
+    //TODO
+}
+
+function defineMaterials(gui){
+    sceneConfigurationObject.material = {
+        Ka: [0.0, 0.0, 0.0],         
+        Kd: [0.0, 0.0, 0.0],         
+        Ks: [0.0, 0.0, 0.0], 
+        shininess: 0         
+    };
+
+    const material = sceneConfigurationObject.material;
+    const materialGui = gui.addFolder("material");
+
+    let materialColors = {
+        ambient: material.Ka.map(c => c * 255),
+        diffuse: material.Kd.map(c => c * 255),
+        specular: material.Ks.map(c => c * 255),
+    };
+
+    materialGui.addColor(materialColors, "ambient").name("Ka").onChange(v => updateMaterialColor("Ka", v));
+    materialGui.addColor(materialColors, "diffuse").name("Kd").onChange(v => updateMaterialColor("Kd", v));
+    materialGui.addColor(materialColors, "specular").name("Ks").onChange(v => updateMaterialColor("Ks", v));
+    materialGui.add(material, "shininess").min(1).max(200).step(1);
+
+
+}
+
+function defineScene(){
+    sceneConfigurationObject.objects = [{
+        name: "Ground",
+        shape: CUBE,
+        modelMatrix: 0 // make Transformations   
+    },
+    {
+        name: "Bunny", 
+        shape: BUNNY,
+        modelMatrix: 0
+        
+    }
+    // OTHER SCENE OBJECTS
+    ];    
+}
+
+function setupMouseEvents(){
+    const { camera } = sceneConfigurationObject;
+    let down = false;
+    let lastX, lastY;
+    window.addEventListener('resize', resizeCanvasToFullWindow);
+
+    window.addEventListener('wheel', function (event) {
+
+        if (!event.altKey && !event.metaKey && !event.ctrlKey) { // Change fovy
+            const factor = 1 - event.deltaY / 1000;
+            camera.fovy = Math.max(1, Math.min(100, camera.fovy * factor));
+        }
+        else if (event.metaKey || event.ctrlKey) {
+            // move camera forward and backwards (shift)
+
+            const offset = event.deltaY / 1000;
+
+            const dir = normalize(subtract(camera.at, camera.eye));
+
+            const ce = add(camera.eye, scale(offset, dir));
+            const ca = add(camera.at, scale(offset, dir));
+
+            // Can't replace the objects that are being listened by dat.gui, only their properties.
+            camera.eye[0] = ce[0];
+            camera.eye[1] = ce[1];
+            camera.eye[2] = ce[2];
+
+            if (event.ctrlKey) {
+                camera.at[0] = ca[0];
+                camera.at[1] = ca[1];
+                camera.at[2] = ca[2];
+            }
+        }
+    });
+    
+    canvas.addEventListener('mousemove', function (event) {
+        if (down) {
+            const dx = event.offsetX - lastX;
+            const dy = event.offsetY - lastY;
+
+            if (dx != 0 || dy != 0) {
+                // Do something here...
+
+                const d = vec2(dx, dy);
+                const axis = vec3(-dy, -dx, 0);
+
+                const rotation = rotate(0.5 * length(d), axis);
+
+                let eyeAt = subtract(camera.eye, camera.at);
+                eyeAt = vec4(eyeAt[0], eyeAt[1], eyeAt[2], 0);
+                let newUp = vec4(camera.up[0], camera.up[1], camera.up[2], 0);
+
+                eyeAt = mult(inCameraSpace(rotation), eyeAt);
+                newUp = mult(inCameraSpace(rotation), newUp);
+
+                console.log(eyeAt, newUp);
+
+                camera.eye[0] = camera.at[0] + eyeAt[0];
+                camera.eye[1] = camera.at[1] + eyeAt[1];
+                camera.eye[2] = camera.at[2] + eyeAt[2];
+
+                camera.up[0] = newUp[0];
+                camera.up[1] = newUp[1];
+                camera.up[2] = newUp[2];
+
+                lastX = event.offsetX;
+                lastY = event.offsetY;
+            }
+
+        }
+    });
+
+    canvas.addEventListener('mousedown', function (event) {
+        down = true;
+        lastX = event.offsetX;
+        lastY = event.offsetY;
+        gl.clearColor(0.2, 0.0, 0.0, 1.0);
+    });
+
+    canvas.addEventListener('mouseup', function (event) {
+        down = false;
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    });
+    
+    
+}
+
+function resizeCanvasToFullWindow() {
+    const {camera} = sceneConfigurationObject;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    camera.aspect = canvas.width / canvas.height;
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+    
+function inCameraSpace(m) {
+    if (!mView) return m;
+    const mInvView = inverse(mView);
+    return mult(mInvView, mult(m, mView));
+}
+
+function setup(shaders) {
+    canvas = document.getElementById('gl-canvas');
+    gl = setupWebGL(canvas);
+
+    CUBE.init(gl);
+    SPHERE.init(gl);
+    BUNNY.init(gl);
+    TORUS.init(gl);
+
+    program = buildProgramFromSources(gl, shaders['shader.vert'], shaders['shader.frag']);
+    const gui = new dat.GUI();
+
+    sceneConfigurationObject = {};
+    datGuiConfigurationObject = {};
+
+    defineCamera(gui);
+    defineOptions(gui, gl);
+    defineLights(gui);
+    defineMaterials(gui);
+    defineScene();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    resizeCanvasToFullWindow();
+    setupMouseEvents();
+
+    window.requestAnimationFrame(render);
     
 
-    // todo
+}
+
+function render(time) {
+    window.requestAnimationFrame(render);
+    const { camera, options, material, objects, lights } = sceneConfigurationObject;
+
+    if (options.depthTest) gl.enable(gl.DEPTH_TEST); else gl.disable(gl.DEPTH_TEST);
+    if (options.backfaceCulling) gl.enable(gl.CULL_FACE); else gl.disable(gl.CULL_FACE);
+    
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.useProgram(program);
+    
+    mView = lookAt(camera.eye, camera.at, camera.up);;
+    const mProjection = perspective(camera.fovy, camera.aspect, camera.near, camera.far);
+    
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_model_view"), false, flatten(STACK.modelView()));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_projection"), false, flatten(mProjection));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_normals"), false, flatten(normalMatrix(STACK.modelView())));
+    gl.uniform1i(gl.getUniformLocation(program, "u_use_normals"), options.normals);
+    SPHERE.draw(gl, program, options.wireframe ? gl.LINES : gl.TRIANGLES);
+    CUBE.draw(gl, program, gl.LINES);
+}
+
+const urls = ['shader.vert', 'shader.frag'];
+
+loadShadersFromURLS(urls).then(shaders => setup(shaders));
+
+// todo
     /*
     GUI todo
         pre: go over code check if implementation is correct
@@ -159,138 +424,3 @@ function setup(shaders) {
     */
 
     // matrices
-    let mView, mProjection;
-
-    let down = false;
-    let lastX, lastY;
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.enable(gl.DEPTH_TEST);
-
-    resizeCanvasToFullWindow();
-
-    window.addEventListener('resize', resizeCanvasToFullWindow);
-
-    window.addEventListener('wheel', function (event) {
-
-
-        if (!event.altKey && !event.metaKey && !event.ctrlKey) { // Change fovy
-            const factor = 1 - event.deltaY / 1000;
-            camera.fovy = Math.max(1, Math.min(100, camera.fovy * factor));
-        }
-        else if (event.metaKey || event.ctrlKey) {
-            // move camera forward and backwards (shift)
-
-            const offset = event.deltaY / 1000;
-
-            const dir = normalize(subtract(camera.at, camera.eye));
-
-            const ce = add(camera.eye, scale(offset, dir));
-            const ca = add(camera.at, scale(offset, dir));
-
-            // Can't replace the objects that are being listened by dat.gui, only their properties.
-            camera.eye[0] = ce[0];
-            camera.eye[1] = ce[1];
-            camera.eye[2] = ce[2];
-
-            if (event.ctrlKey) {
-                camera.at[0] = ca[0];
-                camera.at[1] = ca[1];
-                camera.at[2] = ca[2];
-            }
-        }
-    });
-
-    function inCameraSpace(m) {
-        const mInvView = inverse(mView);
-
-        return mult(mInvView, mult(m, mView));
-    }
-
-    canvas.addEventListener('mousemove', function (event) {
-        if (down) {
-            const dx = event.offsetX - lastX;
-            const dy = event.offsetY - lastY;
-
-            if (dx != 0 || dy != 0) {
-                // Do something here...
-
-                const d = vec2(dx, dy);
-                const axis = vec3(-dy, -dx, 0);
-
-                const rotation = rotate(0.5 * length(d), axis);
-
-                let eyeAt = subtract(camera.eye, camera.at);
-                eyeAt = vec4(eyeAt[0], eyeAt[1], eyeAt[2], 0);
-                let newUp = vec4(camera.up[0], camera.up[1], camera.up[2], 0);
-
-                eyeAt = mult(inCameraSpace(rotation), eyeAt);
-                newUp = mult(inCameraSpace(rotation), newUp);
-
-                console.log(eyeAt, newUp);
-
-                camera.eye[0] = camera.at[0] + eyeAt[0];
-                camera.eye[1] = camera.at[1] + eyeAt[1];
-                camera.eye[2] = camera.at[2] + eyeAt[2];
-
-                camera.up[0] = newUp[0];
-                camera.up[1] = newUp[1];
-                camera.up[2] = newUp[2];
-
-                lastX = event.offsetX;
-                lastY = event.offsetY;
-            }
-
-        }
-    });
-
-    canvas.addEventListener('mousedown', function (event) {
-        down = true;
-        lastX = event.offsetX;
-        lastY = event.offsetY;
-        gl.clearColor(0.2, 0.0, 0.0, 1.0);
-    });
-
-    canvas.addEventListener('mouseup', function (event) {
-        down = false;
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    });
-
-    window.requestAnimationFrame(render);
-
-    function resizeCanvasToFullWindow() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-
-        camera.aspect = canvas.width / canvas.height;
-
-        gl.viewport(0, 0, canvas.width, canvas.height);
-    }
-
-    function render(time) {
-        window.requestAnimationFrame(render);
-
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        gl.useProgram(program);
-
-        mView = lookAt(camera.eye, camera.at, camera.up);
-        STACK.loadMatrix(mView);
-
-        mProjection = perspective(camera.fovy, camera.aspect, camera.near, camera.far);
-
-
-        gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_model_view"), false, flatten(STACK.modelView()));
-        gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_projection"), false, flatten(mProjection));
-        gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_normals"), false, flatten(normalMatrix(STACK.modelView())));
-
-        gl.uniform1i(gl.getUniformLocation(program, "u_use_normals"), options.normals);
-
-        SPHERE.draw(gl, program, options.wireframe ? gl.LINES : gl.TRIANGLES);
-        CUBE.draw(gl, program, gl.LINES);
-    }
-}
-
-const urls = ['shader.vert', 'shader.frag'];
-
-loadShadersFromURLS(urls).then(shaders => setup(shaders));
